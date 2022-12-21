4190802c57a3cc96ecd21f7ae1b166294891b1d7
Hamza TEYEB <teyeb.hamza12@gmail.com>
2022-12-21 21:56:52 +0100

    add head

--- src/components/Testimonial/TestimonialApi.js
+++ src/components/Testimonial/TestimonialApi.js
@@ -2,41 +2,41 @@ const TestimonialApi = [
     {
       id: 1,
       image: "./image/slide1.png",
-      design: "NCD - DESIGN",
+      
       name: "Mevine Thoda",
-      offcer: "Marketing Officer",
+      
       post: "CEO - Marketing",
-      date: "Thoda Department - Mar 4, 2018 - Aug 30, 2021",
+      
       desc: "Marcent Of Vanice and treatment. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam sodales phasellus smauris",
     },
     {
       id: 2,
       image: "./image/slide2.png",
-      design: "Default name",
+     
       name: "Cleanobox",
-      offcer: "Chief Operating Manager",
+     
       post: "Android App Development",
-      date: "via Upwork - Mar 4, 2015 - Aug 30, 2021",
+      
       desc: "When managment is so important. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam sodales phasellus smauris.",
     },
     {
       id: 3,
       image: "./image/slide3.png",
-      design: "Rainbow-Themes",
+     
       name: "Hayla-tn",
-      offcer: "Chief Operating Officer",
-      post: "Android App Development",
-      date: "via Upwork - Mar 4, 2015 - Aug 30, 2021",
+      
+      post: "E-COMMERCE",
+      
       desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam sodales phasellus smauris.",
     },
     {
       id: 4,
       image: "./image/slide4.png",
-      design: "Bound - Trolola",
+     
       name: "Nefertiti",
-      offcer: "Operating Officer",
-      post: "Web App Development",
-      date: "Upwork - Mar 4, 2016 - Aug 30, 2021",
+      
+      post: "cosmetics company",
+      
       desc: "Important fact to nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam sodales phasellus smauris.",
     },
     
